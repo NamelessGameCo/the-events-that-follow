4d682c239ccba520ba4f523dd7cc0c9c3621caa1
Isaiah Mann <contact@isaiahmann.com>
2017-01-02 10:16:15 -0500

    Adding UI prefab and dialogue layout

--- Assets/Scripts/UserInterface/EventsThatFollow/DialogueDisplay.cs
+++ Assets/Scripts/UserInterface/EventsThatFollow/DialogueDisplay.cs
@@ -3,7 +3,28 @@
  * Desc: Controls display of dialogue
  */
 
+using UnityEngine;
+using UnityEngine.UI;
+
 public class DialogueDisplay : UIElement {
+	[SerializeField]
+	Text dialogeText;
+	[SerializeField]
+	Text speakerName;
 
+	public override void Show () {
+		toggleCanvasGroup(canvas, active:true);	
+	}
 
+	public override void Hide () {
+		toggleCanvasGroup(canvas, active:false);
+	}
+
+	public override void SetText (string text) {
+		this.dialogeText.text = text;
+	}
+
+	public void SetSpeaker (string speakerName) {
+		this.speakerName.text = speakerName;
+	}
 }
